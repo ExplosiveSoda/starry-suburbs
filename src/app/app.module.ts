b5e4cdfe986d707def1c2e1ad08355d0dfa47376
Tyler <tyler.mattioli@mt.gov>
2019-09-27 11:42:11 -0600

    added some default layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ContainerComponent } from './container/container.component';
+import { SidebarComponent } from './container/sidebar/sidebar.component';
+import { MapComponent } from './container/map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ContainerComponent,
+    SidebarComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
