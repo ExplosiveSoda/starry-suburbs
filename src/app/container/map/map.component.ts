e2131f7cc32c6c9200d7c454b12fdaa5482516f9
Tyler Mattioli <tylerjoseph.6@gmail.com>
2019-09-28 15:14:57 -0600

    some style stuff and CNAME

--- src/app/container/map/map.component.ts
+++ src/app/container/map/map.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
-
+  public screenHeight: number;
+  public screenWidth: number;
   constructor() { }
 
   ngOnInit() {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+    this.calculateWidth();
+  }
+
+  calculateWidth() {
+    const removeFromWidth = this.screenWidth * 0.2;
+    this.screenWidth = this.screenWidth - removeFromWidth;
+    this.screenWidth = this.screenWidth * 0.6;
   }
 
 }
