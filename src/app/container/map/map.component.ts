39b3822595f293a429a159540265acc68c6b74e7
Tyler <tyler.mattioli@mt.gov>
2019-09-30 13:59:15 -0600

    changed leaflet up

--- src/app/container/map/map.component.ts
+++ src/app/container/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { latLng, tileLayer, imageOverlay, latLngBounds } from 'leaflet';
+import * as L from 'leaflet';
+import { Title } from '@angular/platform-browser';
+// import { latLng, tileLayer, imageOverlay, latLngBounds } from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -10,31 +12,31 @@ export class MapComponent implements OnInit {
   public screenHeight: number;
   public screenWidth: number;
 
-  public imageBounds = latLngBounds([[40.712216, -74.22655], [40.773941, -74.12544]]);
-  public imgOverlay = imageOverlay('../../../assets/images/1030.jpg', this.imageBounds);
+  // public imageBounds = latLngBounds([[40.712216, -74.22655], [40.773941, -74.12544]]);
+  // public imgOverlay = imageOverlay('../../../assets/images/1030.jpg', this.imageBounds);
 
-  options = {
-    layers: [
-      tileLayer('../../../assets/images/1030.jpg', {
-        maxZoom: 18,
-        noWrap: true
-      })
-    ],
-    zoom: 7,
-    center: latLng([ 46.879966, -121.726909 ])
-  };
+  // options = {
+  //   layers: [
+  //     tileLayer('../../../assets/images/1030.jpg', {
+  //       maxZoom: 18,
+  //       noWrap: true
+  //     })
+  //   ],
+  //   zoom: 7,
+  //   center: latLng([ 46.879966, -121.726909 ])
+  // };
 
-  layersControl = {
-    baseLayers: {
-      SeasonX: tileLayer('../../../assets/images/1030.jpg',
-      {
-        maxZoom: 18
-      })
-    },
-    // overlays: {
-    //   'Image Overlay': this.imgOverlay
-    // }
-  };
+  // layersControl = {
+  //   baseLayers: {
+  //     SeasonX: tileLayer('../../../assets/images/1030.jpg',
+  //     {
+  //       maxZoom: 18
+  //     })
+  //   },
+  //   // overlays: {
+  //   //   'Image Overlay': this.imgOverlay
+  //   // }
+  // };
 
   constructor() { }
 
@@ -42,6 +44,7 @@ export class MapComponent implements OnInit {
     this.screenHeight = window.innerHeight;
     this.screenWidth = window.innerWidth;
     this.calculateWidth();
+    this.makeMap();
   }
 
   calculateWidth() {
@@ -50,4 +53,30 @@ export class MapComponent implements OnInit {
     this.screenWidth = this.screenWidth * 0.65;
   }
 
+  makeMap() {
+    const map = L.map('leafletmap', {
+      crs: L.CRS.Simple,
+      minZoom: -6,
+      maxZoom: 0
+    });
+    const bounds = L.latLngBounds([[0, 0], [15000, 15000]]);
+    const image = L.imageOverlay('../../../assets/images/1030.jpg', bounds).addTo(map);
+    map.fitBounds(bounds);
+    const sol = L.latLng([ 7500, 7500 ]);
+    // L.marker(sol).addTo(map);
+    // L.marker(sol, {
+    //   title: 'test'
+    // }).addTo(map);
+    // L.popup()
+    //   .setLatLng([ 7500, 7500 ])
+    //   .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+    //   .openOn(map);
+    const marker = L.marker([7200, 5250], { opacity: 0.01 });
+    marker.bindTooltip('Gotham City', {permanent: true, direction: 'center', className: 'my-labels', offset: [0, 0] }).openTooltip();
+    marker.addTo(map);
+    map.setView( [7500, 7500], -4);
+  }
+
+  // for zooming
+  // https://stackoverflow.com/questions/17382012/is-there-a-way-to-resize-marker-icons-depending-on-zoom-level-in-leaflet
 }
